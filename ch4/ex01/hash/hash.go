2194e6c1001ee8663bf8f217ca389c513ed56f86
github kkk777-7 <kota.kimura0725@gmail.com>
2021-11-26 00:30:05 +0900

    Added hash.go

---
+++ ch4/ex01/hash/hash.go
@@ -0,0 +1,27 @@
+package hash
+
+import "crypto/sha256"
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func totalPopCount(bytes []byte) int {
+	total := 0
+	for _, b := range bytes {
+		total += int(pc[b])
+	}
+	return total
+}
+
+func PopCountDiff(sum1, sum2 [sha256.Size]byte) int {
+	xorBytes := make([]byte, 0, sha256.Size)
+	for i := 0; i < sha256.Size; i++ {
+		xorBytes = append(xorBytes, sum1[i]^sum2[i])
+	}
+	return totalPopCount(xorBytes)
+}

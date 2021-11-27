b00112049c11f8f9565e35bee039f285464b696c
github kkk777-7 <kota.kimura0725@gmail.com>
2021-11-27 14:58:01 +0900

    Updated main.go

--- ch4/ex02/main.go
+++ ch4/ex02/main.go
@@ -2,17 +2,33 @@ package main
 
 import (
 	"bufio"
+	"flag"
+	"fmt"
 	"gobook/ch4/ex02/myhash"
 	"log"
 	"os"
 )
 
+var f string
+
+func init() {
+	flag.StringVar(&f, "H", "256", `type of hash "256" or "384" or "512"`)
+}
+
 func main() {
+	flag.Parse()
+	switch f {
+	case "256", "384", "512":
+	default:
+		fmt.Printf("invalid flag: %s\n", f)
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
 	reader := bufio.NewReader(os.Stdin)
 	bytes, err := reader.ReadBytes('\n')
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	myhash.Hash(bytes[:len(bytes)-1])
+	myhash.Hash(bytes[:len(bytes)-1], f)
 }

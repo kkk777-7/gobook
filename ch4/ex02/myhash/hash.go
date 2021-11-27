312658509a609befdb09dfb759119137f07b5096
github kkk777-7 <kota.kimura0725@gmail.com>
2021-11-27 14:58:19 +0900

    Updated hash.go

--- ch4/ex02/myhash/hash.go
+++ ch4/ex02/myhash/hash.go
@@ -2,11 +2,20 @@ package myhash
 
 import (
 	"crypto/sha256"
+	"crypto/sha512"
 	"fmt"
 )
 
-func Hash(b []byte) {
-
-	sum := sha256.Sum256(b)
-	fmt.Printf("%x\n", sum)
+func Hash(b []byte, s string) {
+	switch s {
+	case "256":
+		sum := sha256.Sum256(b)
+		fmt.Printf("%x\n", sum)
+	case "384":
+		sum := sha512.Sum384(b)
+		fmt.Printf("%x\n", sum)
+	case "512":
+		sum := sha512.Sum512(b)
+		fmt.Printf("%x\n", sum)
+	}
 }

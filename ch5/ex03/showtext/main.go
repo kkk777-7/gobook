377d90f17546215c666de38a0fbcebac49725ae6
github kkk777-7 <kota.kimura0725@gmail.com>
2021-12-05 21:02:43 +0900

    Added main.go

--- ch5/ex03/showtext/main.go
+++ ch5/ex03/showtext/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -13,5 +14,26 @@ func main() {
 		fmt.Fprintf(os.Stderr, "elecount: %v\n", err)
 		os.Exit(1)
 	}
+	searchTextNode(nil, doc)
 
+}
+
+func searchTextNode(stack []string, n *html.Node) {
+	if n.Type == html.ElementNode {
+		stack = append(stack, n.Data)
+	}
+	if n.Type == html.TextNode {
+		chk := stack[len(stack)-1]
+
+		if chk != "script" && chk != "style" {
+			text := strings.TrimSpace(n.Data)
+			if len(text) > 0 {
+				fmt.Printf("<%s>%s</%s>\n", chk, text, chk)
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		searchTextNode(stack, c)
+	}
 }

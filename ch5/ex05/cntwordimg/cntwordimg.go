7c05eceb8799f9c108d5fd6f8bb31470f5c83cd8
github kkk777-7 <kota.kimura0725@gmail.com>
2021-12-06 20:45:04 +0900

    Added cntwordimg.go

---
+++ ch5/ex05/cntwordimg/cntwordimg.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	words, images := countWordsAndImages(doc)
+	fmt.Printf("words: %v\nimages: %v\n", words, images)
+}
+
+func countWordsAndImages(n *html.Node) (words, images int) {
+	if n.Type == html.ElementNode && n.Data == "img" {
+		images++
+	}
+
+	scanner := bufio.NewScanner(strings.NewReader(n.Data))
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		words++
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		w, i := countWordsAndImages(c)
+		words += w
+		images += i
+	}
+	return words, images
+}

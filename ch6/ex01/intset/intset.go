e374c61570a72feb7b95c83af160b909b2983f1c
github kkk777-7 <kota.kimura0725@gmail.com>
2021-12-11 16:58:13 +0900

    Updated intset.go

--- ch6/ex01/intset/intset.go
+++ ch6/ex01/intset/intset.go
@@ -11,28 +11,26 @@ type IntSet struct {
 
 // Has reports Whether the set contains the non-negative value x.
 func (s *IntSet) Has(x int) bool {
-	Word, bit := x/64, uint(x%64)
-	return Word < len(s.Words) && s.Words[Word]&(1<<bit) != 0
+	word, bit := x/64, uint(x%64)
+	return word < len(s.Words) && s.Words[word]&(1<<bit) != 0
 }
 
 // Add adds the non-negative value x to the set.
 func (s *IntSet) Add(x int) {
-	Word, bit := x/64, uint(x%64)
-	for Word >= len(s.Words) {
-		fmt.Printf("%b\n", s.Words)
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.Words) {
 		s.Words = append(s.Words, 0)
-		fmt.Printf("%b\n", s.Words)
 	}
-	s.Words[Word] |= 1 << bit // s.Words[Word] = s.Words[Word] | 1 << bit
+	s.Words[word] |= 1 << bit // s.Words[Word] = s.Words[Word] | 1 << bit
 }
 
 // UnionWith sets s to the union of s and t.
 func (s *IntSet) UnionWith(t *IntSet) {
-	for i, tWord := range t.Words {
+	for i, tword := range t.Words {
 		if i < len(s.Words) {
-			s.Words[i] |= tWord
+			s.Words[i] |= tword
 		} else {
-			s.Words = append(s.Words, tWord)
+			s.Words = append(s.Words, tword)
 		}
 	}
 }
@@ -41,12 +39,12 @@ func (s *IntSet) UnionWith(t *IntSet) {
 func (s *IntSet) String() string {
 	var buf bytes.Buffer
 	buf.WriteByte('{')
-	for i, Word := range s.Words {
-		if Word == 0 {
+	for i, word := range s.Words {
+		if word == 0 {
 			continue
 		}
 		for j := 0; j < 64; j++ {
-			if Word&(1<<uint(j)) != 0 {
+			if word&(1<<uint(j)) != 0 {
 				if buf.Len() > len("{") {
 					buf.WriteByte(' ')
 				}
@@ -56,4 +54,31 @@ func (s *IntSet) String() string {
 	}
 	buf.WriteByte('}')
 	return buf.String()
+}
+
+// Return the number of elements.
+func (s *IntSet) Len() int {
+	num := len(s.Words)
+	return num
+}
+
+// Remove x from the set.
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	s.Words[word] &= 0 << bit
+}
+
+// Remove all elements from the set.
+func (s *IntSet) Clear() {
+	for i := range s.Words {
+		for j := 0; j < 64; j++ {
+			s.Words[i] &= 0 << uint(j)
+		}
+	}
+}
+
+// Returns a copy of the set.
+func (s *IntSet) Copy() *IntSet {
+	res := s
+	return res
 }
